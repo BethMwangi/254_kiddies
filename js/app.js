be3a4497d1b8e829e3515e2a2139d33dd7126b81
BethMwangi <wanjikumwangib@gmail.com>
2016-06-20 12:33:53 +0300

    modified and added footer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 $(document).foundation();
+
+
+var placeholder = document.querySelectorAll('.placeholder'),
+    submit = document.querySelectorAll('.submit'),
+    input = document.getElementById('input');
+
+function clean(event) {
+  if(event.keyCode === 13) {
+    moveBtn();
+  }
+  placeholder[0].style.opacity = "0";
+  placeholder[0].innerHTML = "Check your inbox!";
+}
+
+input.addEventListener('keydown', function() {
+  placeholder[0].style.opacity = "0";
+});
+
+submit[0].addEventListener('click', function() {
+  moveBtn();
+});
+
+function moveBtn() {
+  submit[0].classList.add('move-btn');
+  placeholder[0].style.opacity = "0";
+  setTimeout(function() {
+    placeholder[0].innerHTML = "Check your inbox!"
+    submit[0].textContent = "Sweet";
+    placeholder[0].classList.add('placeholder-active');
+    input.value = "";
+  }, 500);
+}
